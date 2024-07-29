13c39a737f38ba0dd18fb3bc06402503e402e50b
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-07-29 21:38:03 +0300

    görev tamamlandı

---
+++ src/main/java/com/workintech/zoo/exceptions/ZooException.java
@@ -0,0 +1,26 @@
+package com.workintech.zoo.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+
+
+
+public class ZooException extends RuntimeException {
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    private HttpStatus httpStatus;
+
+    public ZooException(String message,HttpStatus httpStatus){
+    super(message);
+    this.httpStatus=httpStatus;
+}
+
+
+}
